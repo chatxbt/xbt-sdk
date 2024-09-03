3f74f04d08ff5f7b9e4a65da14c224836a18220c
okolilemuel <undefined>
2024-09-03 15:38:14 +0100

    Add support for default export

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import "./src/config/logger";
 
-export { cryptoUtils } from "./src/modules/crypto/utils";
+import { cryptoUtils } from "./src/modules/crypto/utils";
+import Http from "./src/modules/http";
+import KV from "./src/modules/store/kv";
+import Secrets from "./src/modules/store/secrets";
 
-export { default as Http } from "./src/modules/http";
-export { default as KV } from "./src/modules/store/kv";
-export { default as Secrets } from "./src/modules/store/secrets";
+export { cryptoUtils, Http, KV, Secrets };
+
+export default {
+  cryptoUtils,
+  Http,
+  KV,
+  Secrets
+};
